1daaa79d4207a36faaecec0a0a1b3533e0a259ad
andrewhamili <rockr1521@gmail.com>
2017-03-25 19:59:58 +0800

    Created the Telerik WinForms version of the Login page. No codes yet.

---
+++ CEUPreventiveMaintenanceSystem/Login.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Login
+    Inherits Telerik.WinControls.UI.RadForm
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VisualStudio2012DarkTheme1 = New Telerik.WinControls.Themes.VisualStudio2012DarkTheme()
+        Me.txtUsername = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadTextBox1 = New Telerik.WinControls.UI.RadTextBox()
+        Me.RadLabel1 = New Telerik.WinControls.UI.RadLabel()
+        Me.RadLabel2 = New Telerik.WinControls.UI.RadLabel()
+        Me.btnLogin = New Telerik.WinControls.UI.RadButton()
+        CType(Me.txtUsername, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnLogin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtUsername
+        '
+        Me.txtUsername.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.txtUsername.Location = New System.Drawing.Point(190, 129)
+        Me.txtUsername.Name = "txtUsername"
+        Me.txtUsername.Size = New System.Drawing.Size(142, 28)
+        Me.txtUsername.TabIndex = 0
+        Me.txtUsername.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadTextBox1
+        '
+        Me.RadTextBox1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadTextBox1.Location = New System.Drawing.Point(190, 180)
+        Me.RadTextBox1.Name = "RadTextBox1"
+        Me.RadTextBox1.NullText = "[password]"
+        Me.RadTextBox1.PasswordChar = Global.Microsoft.VisualBasic.ChrW(9679)
+        Me.RadTextBox1.Size = New System.Drawing.Size(142, 28)
+        Me.RadTextBox1.TabIndex = 1
+        Me.RadTextBox1.ThemeName = "VisualStudio2012Dark"
+        Me.RadTextBox1.UseSystemPasswordChar = True
+        '
+        'RadLabel1
+        '
+        Me.RadLabel1.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel1.Location = New System.Drawing.Point(36, 129)
+        Me.RadLabel1.Name = "RadLabel1"
+        Me.RadLabel1.Size = New System.Drawing.Size(148, 25)
+        Me.RadLabel1.TabIndex = 2
+        Me.RadLabel1.Text = "CEU Gmail Account"
+        Me.RadLabel1.ThemeName = "VisualStudio2012Dark"
+        '
+        'RadLabel2
+        '
+        Me.RadLabel2.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.RadLabel2.Location = New System.Drawing.Point(108, 180)
+        Me.RadLabel2.Name = "RadLabel2"
+        Me.RadLabel2.Size = New System.Drawing.Size(76, 25)
+        Me.RadLabel2.TabIndex = 2
+        Me.RadLabel2.Text = "Password"
+        Me.RadLabel2.ThemeName = "VisualStudio2012Dark"
+        '
+        'btnLogin
+        '
+        Me.btnLogin.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.btnLogin.Location = New System.Drawing.Point(157, 254)
+        Me.btnLogin.Name = "btnLogin"
+        Me.btnLogin.Size = New System.Drawing.Size(120, 34)
+        Me.btnLogin.TabIndex = 3
+        Me.btnLogin.Text = "Login"
+        Me.btnLogin.ThemeName = "VisualStudio2012Dark"
+        '
+        'Login
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(386, 318)
+        Me.Controls.Add(Me.btnLogin)
+        Me.Controls.Add(Me.RadLabel2)
+        Me.Controls.Add(Me.RadLabel1)
+        Me.Controls.Add(Me.RadTextBox1)
+        Me.Controls.Add(Me.txtUsername)
+        Me.Font = New System.Drawing.Font("Segoe UI", 12.0!)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Login"
+        '
+        '
+        '
+        Me.RootElement.ApplyShapeToControl = True
+        Me.Text = "Login"
+        Me.ThemeName = "VisualStudio2012Dark"
+        CType(Me.txtUsername, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadTextBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.RadLabel2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnLogin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VisualStudio2012DarkTheme1 As Telerik.WinControls.Themes.VisualStudio2012DarkTheme
+    Friend WithEvents txtUsername As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadTextBox1 As Telerik.WinControls.UI.RadTextBox
+    Friend WithEvents RadLabel1 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents RadLabel2 As Telerik.WinControls.UI.RadLabel
+    Friend WithEvents btnLogin As Telerik.WinControls.UI.RadButton
+End Class
+
